b9073276a0ffdb4fbe9c9f5f83d9aeba61381a2b
RightAccept <djfltrhkdeo46@gmail.com>
2023-05-27 06:05:28 +0900

    23/05/27 알고리즘 2문제 해결

--- algorithm/src/algorithm/StringCut.java
+++ algorithm/src/algorithm/StringCut.java
@@ -1,7 +1,34 @@
 package algorithm;
 
+import java.util.Scanner;
+
 public class StringCut {
 	public static void main(String[] args) {
-		
+		/*
+		 * 문자열 my_str과 n이 매개변수로 주어질 때, my_str을 길이 n씩 잘라서 저장한 배열을 return하도록 solution 함수를 완성해주세요.
+		 * 
+		 * 제한사항
+		 * 1 ≤ my_str의 길이 ≤ 100
+		 * 1 ≤ n ≤ my_str의 길이
+		 * my_str은 알파벳 소문자, 대문자, 숫자로 이루어져 있습니다.
+		 */
+		Solution s = new Solution();
+
+		String my_str = "";
+		int cut;
+		Scanner sc = new Scanner(System.in);
+
+		System.out.print("문자열 입력 : ");
+		my_str = sc.next();
+		System.out.print("자를 수 입력 : ");
+		cut = sc.nextInt();
+
+		String[] answer = s.StringCut(my_str, cut);
+
+		for (int i = 0; i < answer.length; i++) {
+			System.out.println(answer[i].toString());
+		}
+
+		sc.close();
 	}
 }
--- algorithm/src/algorithm/StringNumChange.java
+++ algorithm/src/algorithm/StringNumChange.java
@@ -1,7 +1,33 @@
 package algorithm;
 
+import java.util.Scanner;
+
 public class StringNumChange {
 	public static void main(String[] args) {
+		/*
+		 * 영어가 싫은 머쓱이는 영어로 표기되어있는 숫자를 수로 바꾸려고 합니다.
+		 * 문자열 numbers가 매개변수로 주어질 때,
+		 * numbers를 정수로 바꿔 return 하도록 solution함수를 완성해 주세요.
+		 * 
+		 * 제한사항
+		 * numbers는 소문자로만 구성되어 있습니다.
+		 * numbers는 "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" 들이 공백 없이 조합되어 있습니다.
+		 * 1 ≤ numbers의 길이 ≤ 50
+		 * "zero"는 numbers의 맨 앞에 올 수 없습니다.
+		 */
+		Solution s = new Solution();
 		
+		Scanner sc = new Scanner(System.in);
+		String numbers = "";
+		long answer;
+		
+		System.out.print("출력할 숫자 \"영어로 입력\" : ");
+		numbers = sc.next();
+		
+		answer = s.StringNumChange(numbers);
+		
+		System.out.println(answer);
+		
+		sc.close();
 	}
 }
