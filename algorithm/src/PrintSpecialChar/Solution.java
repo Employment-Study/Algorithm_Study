1d2b4e0282487154049af122017ef65ab9700853
RightAccept <djfltrhkdeo46@gmail.com>
2023-06-04 17:56:26 +0900

    문제 추가

---
+++ algorithm/src/PrintSpecialChar/Solution.java
@@ -0,0 +1,13 @@
+package PrintSpecialChar;
+
+class Solution {
+    public int[] solution(int[] arr) {
+    	/*
+    	 * 특수문자 출력하기
+    	 * 다음과 같이 출력하도록 코드를 작성해 주세요
+    	 * !@#$%^&*(\'"<>?:;
+    	 */
+        int[] answer = {};
+        return answer;
+    }
+}
--- algorithm/src/arrlength2power/Solution.java
+++ algorithm/src/arrlength2power/Solution.java
@@ -2,6 +2,15 @@ package arrlength2power;
 
 class Solution {
     public int[] solution(int[] arr) {
+    	/*
+    	 * 배열의 길이를 2의 거듭제곱으로 만들기
+    	 * 정수 배열 arr이 매개변수로 주어집니다. arr의 길이가 2의 정수 거듭제곱이 되도록 arr 뒤에 정수 0을 추가하려고 합니다.
+    	 * arr에 최소한의 개수로 0을 추가한 배열을 return 하는 solution 함수를 작성해 주세요
+    	 * 
+    	 * 입출력 예
+    	 * arr = {1, 2, 3, 4, 5, 6}	result = {1, 2, 3, 4, 5, 6, 0, 0}
+    	 * arr = {58, 172, 746, 89}	result = {58, 172, 746, 89}
+    	 */
         int[] answer = {};
         return answer;
     }
