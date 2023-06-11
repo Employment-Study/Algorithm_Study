432f593b848f74e70944da921060867e28f94ac2
eam ji hun <133019246+e1mji@users.noreply.github.com>
2023-06-11 19:56:58 +0900

    6월 둘째주 문제 금토  "커피심부름 "
    문제 설명
    팀의 막내인 철수는 아메리카노와 카페 라테만 판매하는 카페에서 팀원들의 커피를 사려고 합니다. 아메리카노와 카페 라테의 가격은 차가운 것과 뜨거운 것 상관없이 각각 4500, 5000원입니다. 각 팀원에게 마실 메뉴를 적어달라고 하였고, 그 중에서 메뉴만 적은 팀원의 것은 차가운 것으로 통일하고 "아무거나"를 적은 팀원의 것은 차가운 아메리카노로 통일하기로 하였습니다.
    
    각 직원이 적은 메뉴가 문자열 배열 order로 주어질 때, 카페에서 결제하게 될 금액을 return 하는 solution 함수를 작성해주세요. order의 원소는 아래의 것들만 들어오고, 각각의 의미는 다음과 같습니다.
    
    
    
    order의 원소	의미
    "iceamericano", "americanoice"	차가운 아메리카노
    "hotamericano", "americanohot"	따뜻한 아메리카노
    "icecafelatte", "cafelatteice"	차가운 카페 라테
    "hotcafelatte", "cafelattehot"	따뜻한 카페 라테
    "americano"	아메리카노
    "cafelatte"	카페 라테
    "anything"	아무거나

--- Ex02.java
+++ Ex02.java
@@ -1,48 +1,41 @@
-package day04;
+package day06;
 
-import java.util.Arrays;
-
-/*
- * 예제 1번의 arr의 길이는 6입니다. arr의 길이를 2의 정수 거듭제곱으로 만드는 방법은 0을 2개, 
- * 10개, 26개,..., 추가하는 방법이 있고 그중 최소한으로 0을 추가하는 방법은 2개를 추가하는 것입니다. 
- * 따라서 [1, 2, 3, 4, 5, 6, 0, 0]을 return 합니다.
- */
 public class Ex02 {
-	
-	 public static int[] solution(int[] arr) {
-		 	//입력된 arr배열의 변수를 새로 넣을 공간
-	    	int length = arr.length;
-	    	
-	    	//length의 길이와 같아지는 동안 곱해줄 변수 1초기화 
-	    	int nextTwo = 1;
-	    	
-	    	//length의 길이과 같아질때까지 2곱해주기 
-	    	while(nextTwo< length) {
-	    		nextTwo *=2;
-	    	}
-	    	//zero변수 = 2-2
-	    	int zero = nextTwo - length;
-	    	
-	    	//0와과 length 변수를 인덱스요소에 더한 새로운 extend 배열 
-	    	int[] extend = new int[zero+length];
-	    	//반복문으로 extend 인덱스에 넣어준다.
-	    	for(int i =0; i<length; i++) {
-	    		extend[i] = arr[i];
-	    	}
-	    	
-	    	//추가 해야할 0의 개수만큼 0을 추가.
-	    	for(int i =length; i<length + zero; i++) {
-	    			extend[i] =0;
-	    	}
-	    	return extend;
-	    }
- 
-	public static void main(String[] args) {
-		int[] arr = {1,2,3,4,5,6};
-		int[] num = solution(arr);
+	/*
+	 * 차 / 뜨 상관없이 
+		아메리카노 4500
+		카페라떼 5000
 		
-		System.out.println(Arrays.toString(num));
-	}
+		팀원 마실 메뉴 : 
+		if "카페라테"  = 차가운 카페라떼
+		if "아무거나"  = 차가운 아메리카노
+		
+		결제하게 될 금액 return  int total = 
+	 */
 	
+	public int solution(String[] order) {
+		int americanoPrice = 4500;
+		int cafelattePrice = 5000; 
+        int total = 0;
+
+        for(int i =0; i < order.length; i++) {
+            String menu = order[i];
+        	if (menu != null && menu.equals("anything")) {
+        		total += americanoPrice;
+        	}
+        	else if (menu!= null && menu.contains("americano")) {
+        		total +=americanoPrice;
+        	}
+        	else if (menu != null && menu.contains("cafelatte")) {
+        		total += cafelattePrice;
+        	}
+        }	
+        
+        return total;
+    }
+	
+	public static void main(String[] args) {
+		
+	}
 
 }
