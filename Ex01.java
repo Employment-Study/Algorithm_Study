dc0f24ca2fcad97963c302cd61f69856806ddb0e
eam ji hun <133019246+e1mji@users.noreply.github.com>
2023-06-13 21:31:03 +0900

    6월 3주차 월화 "qr 코드"
    qr code
    문제 설명
    두 정수 q, r과 문자열 code가 주어질 때, code의 각 인덱스를 q로 나누었을 때 
    나머지가 r인 위치의 문자를 앞에서부터 순서대로 이어 붙인 문자열을 return 하는 solution 함수를 작성해 주세요.
    
    입출력 예
    q	r	code	                         result
    3	1	"qjnwezgrpirldywt"  	"jerry"
    1	0	"programmers"	        "programmers"

--- Ex01.java
+++ Ex01.java
@@ -1,27 +1,24 @@
-package day06;
+package day07;
 
 public class Ex01 {
-	  static int solution(String my_string) {
-	    	int answer = 0;
-	    	String[] arr =  my_string.split(" ");
-	    	answer = Integer.parseInt(arr[0]);
-	    	for(int i =0; i<arr.length; i++) {
-	        	if(i%2!= 0) {
-		    		if(arr[i].equals("+")) {
-		    			answer +=  Integer.parseInt(arr[i+1]);
-		    		}
-		    		else if (arr[i].equals("-")) {
-		    			answer -= Integer.parseInt(arr[i+1]);
-		    		}
-		    	}
+	    static String solution(int q, int r, String code) {
+	    	String answer = "";
+	    	for(int i =0; i<code.length(); i++) {
+	    		//i의 요소가 q와 나눈 값이 r과 일치하면 
+	    		if(i % q == r) {
+	    			//answer에 code의 i값을 빼서 추가 
+	    			answer +=code.charAt(i);
+	    		}
 	    	}
 	    	return answer;
-	  }
-	  
+	}
 	public static void main(String[] args) {
-		String my_string = "3 + 4";
-		System.out.println(solution(my_string));
+		int q=3;
+		int r=1;
+		String code = "alsdodfl";
+		String s1 = solution(q, r, code);
+		System.out.println(s1);
+		
 		
 	}
-
 }
