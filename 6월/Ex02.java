058ee648da0c9d61a64ead33c2c407f8bb0093ce
eam ji hun <133019246+e1mji@users.noreply.github.com>
2023-06-14 22:21:31 +0900

    구슬 나누기 수정(bigInteger)

---
+++ 6월/Ex02.java
@@ -0,0 +1,35 @@
+package day07;
+
+import java.math.BigInteger;
+
+public class Ex02 {
+	
+    static BigInteger  solution(int balls, int share) {
+    	//만약 머쓱이가 가지고있는 구슬이 친구한테 나눠줄 구슬보다 적다면 0
+        if (balls < share) return BigInteger.ZERO;
+        BigInteger son = BigInteger.ONE;	 //분자	
+        BigInteger mom = BigInteger.ONE;	//분모
+        
+        // 머쓱이가 5개를 가지고 있고 친구한테 3개 나누어준다고 가정하면 분자는 5x4x3
+        for(int i=balls; i>balls-share; i--) {
+        	son = son.multiply(BigInteger.valueOf(i));
+        }
+        // 분모는 3x2x1
+        for (int i = share; i>=2; i--) {
+        	mom = mom.multiply(BigInteger.valueOf(i));
+        }
+        // 60 / 6	
+        BigInteger answer = son.divide(mom);
+        
+        return  answer;
+    }
+
+	public static void main(String[] args) {
+		int balls = 5;
+		int share = 3;
+		BigInteger answer = solution(balls, share);
+		System.out.println(answer);
+		
+	}
+
+}
