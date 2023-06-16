7d0cd8ae4b02a1143c55c8eb7921c48a00c9829e
AhnJiHye-coder <dkswlgP224677@gmail.com>
2023-06-16 22:09:51 +0900

    Study04

---
+++ Study04/src/Quiz04/Solution1.java
@@ -0,0 +1,27 @@
+package Quiz04;
+
+//pr code
+// 월, 화
+//class Solution {
+// public String solution(int q, int r, String code) {
+//     String answer = "";
+//     // StringBuilder은 변경 가능한 문자열을 만들어주는데
+//     // 문자를 합칠때 사용한다
+//     // StringBuilder을 사용할때는 append() 인자를 사용하여
+//     // 연결하고자 하는 문자를 넣어주는데 A에서 B의 문자를 연결하겠다는 의미
+//     StringBuilder sb = new StringBuilder();
+//     for(int i = 0 ; i < code.length() ; i++){
+//         if(i % q == r){
+//             sb.append(code.charAt(i));
+//         	}
+//     	}
+//     // StringBuilder을 출력할떄는 반드시 toString()을 붙여야 한다
+//     	answer = sb.toString();
+//     	return answer;
+// 	}
+//}
+
+
+public class Solution1 {
+
+}
