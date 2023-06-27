2616291054b07d456972cf6410b7c57f8016e7a1
RightAccept <djfltrhkdeo46@gmail.com>
2023-06-27 14:07:37 +0900

    세 개의 구분자, 빈 배열 추가, 삭제 풀이 완료 / 영어 끝말잇기, 직사각형 넓이 추가

--- algorithm/src/emptyArray/Solution.java
+++ algorithm/src/emptyArray/Solution.java
@@ -1,23 +1,53 @@
 package emptyArray;
 
+import java.util.ArrayList;
+
 class Solution {
-    public int[] solution(int[] arr, boolean[] flag) {
-    	/*
-    	 * 아무 원소도 들어있지 않은 빈 배열 X가 있습니다.
-    	 * 길이가 같은 정수 배열 arr과 boolean 배열 flag가 매개변수로 주어질 때, flag를 차례대로 순회하며 flag[i]가 true라면 X의 뒤에 arr[i]를 arr[i] × 2 번 추가하고,
-    	 * flag[i]가 false라면 X에서 마지막 arr[i]개의 원소를 제거한 뒤 X를 return 하는 solution 함수를 작성해 주세요.
-    	 * 
-    	 * 입출력 예
-    	 * arr : [3, 2, 4, 1, 3], flag : [true, false, true, false, false], result : [3, 3, 3, 3, 4, 4, 4, 4]
-    	 * 
-    	 * 입출력 예 설명
-    	 * 0	true	3	[3, 3, 3, 3, 3, 3]
-    	 * 1	false	2	[3, 3, 3, 3]
-    	 * 2	true	4	[3, 3, 3, 3, 4, 4, 4, 4, 4, 4, 4, 4]
-    	 * 3	false	1	[3, 3, 3, 3, 4, 4, 4, 4, 4, 4, 4]
-    	 * 4	false	3	[3, 3, 3, 3, 4, 4, 4, 4]
-    	 */
-        int[] answer = {};
-        return answer;
-    }
+	public int[] solution(int[] arr, boolean[] flag) {
+		/*
+		 * 아무 원소도 들어있지 않은 빈 배열 X가 있습니다. 길이가 같은 정수 배열 arr과 boolean 배열 flag가 매개변수로 주어질 때,
+		 * flag를 차례대로 순회하며 flag[i]가 true라면 X의 뒤에 arr[i]를 arr[i] × 2 번 추가하고, flag[i]가
+		 * false라면 X에서 마지막 arr[i]개의 원소를 제거한 뒤 X를 return 하는 solution 함수를 작성해 주세요.
+		 * 
+		 * 입출력 예 arr : [3, 2, 4, 1, 3], flag : [true, false, true, false, false], result
+		 * : [3, 3, 3, 3, 4, 4, 4, 4]
+		 * 
+		 * 입출력 예 설명 0 true 3 [3, 3, 3, 3, 3, 3] 1 false 2 [3, 3, 3, 3] 2 true 4 [3, 3,
+		 * 3, 3, 4, 4, 4, 4, 4, 4, 4, 4] 3 false 1 [3, 3, 3, 3, 4, 4, 4, 4, 4, 4, 4] 4
+		 * false 3 [3, 3, 3, 3, 4, 4, 4, 4]
+		 */
+		int[] answer = {};
+		ArrayList<Integer> list = new ArrayList<Integer>();
+
+		for (int i = 0; i < flag.length; i++) {
+			if (flag[i]) {
+				for (int j = 0; j < arr[i] * 2; j++) {
+					list.add(arr[i]);
+				}
+			} else {
+				for (int j = 0; j < arr[i]; j++) {
+					int index = list.size() - 1;
+					list.remove(index);
+				}
+			}
+		}
+		answer = new int[list.size()];
+		for (int i = 0; i < list.size(); i++) {
+			answer[i] = list.get(i);
+		}
+
+		return answer;
+	}
+
+	public static void main(String[] args) {
+		Solution s = new Solution();
+		int[] arr = { 3, 2, 4, 1, 3 };
+		boolean[] flag = { true, false, true, false, false };
+		int[] answer;
+
+		answer = s.solution(arr, flag);
+		for (int num : answer) {
+			System.out.println(num);
+		}
+	}
 }
--- algorithm/src/threeSeparator/Solution.java
+++ algorithm/src/threeSeparator/Solution.java
@@ -1,19 +1,43 @@
 package threeSeparator;
 
 class Solution {
-    public String[] solution(String myStr) {
-    	/*
-    	 * 임의의 문자열이 주어졌을 때 문자 "a", "b", "c"를 구분자로 사용해 문자열을 나누고자 합니다
-    	 * 예를 들어 주어진 문자열이 "baconlettucetomato"라면 나눠진 문자열 목록은 ["onlettu", "etom", "to"] 가 됩니다.
-    	 * 문자열 myStr이 주어졌을 때 위 예시와 같이 "a", "b", "c"를 사용해 나눠진 문자열을 순서대로 저장한 배열을 return 하는 solution 함수를 완성해 주세요.
-    	 * 단, 두 구분자 사이에 다른 문자가 없을 경우에는 아무것도 저장하지 않으며, return할 배열이 빈 배열이라면 ["EMPTY"]를 return 합니다.
-    	 * 
-    	 * 입출력 예
-    	 * myStr : baconlettucetomato result : ["onlettu", "etom", "to"]
-    	 * myStr : abcd result : ["d"]
-    	 * myStr : cabab result : ["EMPTY"]
-    	 */
-        String[] answer = {};
-        return answer;
-    }
+	public String[] solution(String myStr) {
+		/*
+		 * 임의의 문자열이 주어졌을 때 문자 "a", "b", "c"를 구분자로 사용해 문자열을 나누고자 합니다 예를 들어 주어진 문자열이
+		 * "baconlettucetomato"라면 나눠진 문자열 목록은 ["onlettu", "etom", "to"] 가 됩니다. 문자열
+		 * myStr이 주어졌을 때 위 예시와 같이 "a", "b", "c"를 사용해 나눠진 문자열을 순서대로 저장한 배열을 return 하는
+		 * solution 함수를 완성해 주세요. 단, 두 구분자 사이에 다른 문자가 없을 경우에는 아무것도 저장하지 않으며, return할 배열이
+		 * 빈 배열이라면 ["EMPTY"]를 return 합니다.
+		 * 
+		 * 입출력 예 myStr : baconlettucetomato result : ["onlettu", "etom", "to"] myStr :
+		 * abcd result : ["d"] myStr : cabab result : ["EMPTY"]
+		 */
+		String[] answer = {};
+		String[] tmp;
+		int index = 0;
+
+		myStr = myStr.replace("a", "_");
+		myStr = myStr.replace("b", "_");
+		myStr = myStr.replace("c", "_");
+		tmp = myStr.split("_");
+
+		for (int i = 0; i < tmp.length; i++) {
+			if (!tmp[i].isEmpty()) {
+				index++;
+			}
+		}
+		answer = new String[index];
+		index = 0;
+
+		for (int i = 0; i < tmp.length; i++) {
+			if (!tmp[i].isEmpty()) {
+				answer[index++] = tmp[i];
+			}
+		}
+		if (answer.length == 0) {
+			answer = new String[1];
+			answer[0] = "EMPTY";
+		}
+		return answer;
+	}
 }
